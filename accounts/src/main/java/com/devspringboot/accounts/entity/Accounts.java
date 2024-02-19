9c42c99e3f1d2bbf6b3c7f1b3e9fabb8180d5125
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2024-02-19 21:23:47 +0530

    Accounts - JpaRepository implemented

---
+++ accounts/src/main/java/com/devspringboot/accounts/entity/Accounts.java
@@ -0,0 +1,19 @@
+package com.devspringboot.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class Accounts extends BaseEntity {
+
+    @Column(name = "customer_id")
+    private long customerId;
+    @Column(name = "account_number")
+    private long accountNumber;
+    @Column(name = "account_type")
+    private String accountType;
+    @Column(name = "branch_address")
+    private String branchAddress;
+}
