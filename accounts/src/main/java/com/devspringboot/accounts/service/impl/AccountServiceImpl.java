af14fd458b0a5c25d7191a7630502937e6f127b7
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2024-02-25 19:40:22 +0530

    Accounts - audit columns service fixed

--- accounts/src/main/java/com/devspringboot/accounts/service/impl/AccountServiceImpl.java
+++ accounts/src/main/java/com/devspringboot/accounts/service/impl/AccountServiceImpl.java
@@ -36,8 +36,6 @@ public class AccountServiceImpl implements IAccountService {
         if (optionalCustomer.isPresent()){
             throw new CustomerAlreadyExistsException("Customer already registered with given email or mobileNumber");
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -116,8 +114,6 @@ public class AccountServiceImpl implements IAccountService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
         return newAccount;
     }
 }
