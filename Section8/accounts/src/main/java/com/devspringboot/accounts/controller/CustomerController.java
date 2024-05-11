16e08e1e18e33a65f003ec768be85d56c8252210
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2024-05-11 23:25:37 +0530

    ongoing -

---
+++ Section8/accounts/src/main/java/com/devspringboot/accounts/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package com.devspringboot.accounts.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(
+        name = "CRUD REST API for Customers in EazyBank",
+        description = "Rest APIs in EazyBank to FETCH customer details"
+)
+@RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@Validated
+public class CustomerController {
+}
