ca5b9a3516fc06798208489ee67dc1d0481e3c9d
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2024-06-08 16:48:20 +0530

    Custom Routing - added

---
+++ Section9/accounts/src/main/java/com/devspringboot/accounts/service/client/LoansFeignClient.java
@@ -0,0 +1,20 @@
+package com.devspringboot.accounts.service.client;
+
+import com.devspringboot.accounts.dto.LoansDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("loans")
+public interface LoansFeignClient {
+    @GetMapping(value = "/api/fetch", consumes = "application/json")
+    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestParam String mobileNumber);
+}
